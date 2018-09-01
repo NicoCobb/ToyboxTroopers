4b18d79a073521ace7cc62403f76f6d13742e911
lessuknow <chenc18@rpi.edu>
2018-09-01 03:25:00 -0400

    Made cube physics that hopefully work; code is decently docuemtned @ the top. Random bug where sometimes a bullet will just, nuke a tower and as such everything else. hopefully that doesn't happen OR ITS A FEATURE HAHA

---
+++ ToyboxTroopers/Assets/Scripts/Debug_TestBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//p to shoot the bullet it dies after hitting anything for debug
+
+public class Debug_TestBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.P) && gameObject.GetComponent<Rigidbody>().velocity==Vector3.zero)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 20000);
+        }
+        
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
