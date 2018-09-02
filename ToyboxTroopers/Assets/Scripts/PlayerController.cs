ce1482659fb68ff518c817dc53b2552518174fee
Nico Cobb <cobbn@rpi.edu>
2018-09-02 13:51:50 -0400

    fix ControllerTestingScene merge conflict

--- ToyboxTroopers/Assets/Scripts/PlayerController.cs
+++ ToyboxTroopers/Assets/Scripts/PlayerController.cs
@@ -35,15 +35,14 @@ public class PlayerController : MonoBehaviour {
 
     private void movementManager(float horizontal, float vertical)
     {
-
-        Vector3 targetMovement = new Vector3(horizontal, 0.0f, vertical);
-        targetMovement = playerCam.transform.TransformDirection(targetMovement);
-        rb.AddForce(targetMovement * speed);
+        //Vector3 targetMovement = new Vector3(horizontal, 0.0f, vertical);
+        //targetMovement = playerCam.transform.TransformDirection(targetMovement);
+        //rb.AddForce(targetMovement * speed);
 
         //messing around with using torque instead since it's a ball
         //i think it can potentially feel better this way but requires much more tuning
-        //Vector3 targetTorque = new Vector3(vertical, 0.0f, -horizontal);
-        //targetTorque = playerCam.transform.TransformDirection(targetTorque);
-        //rb.AddTorque(targetTorque * speed);
+        Vector3 targetTorque = new Vector3(vertical, 0.0f, -horizontal);
+        targetTorque = playerCam.transform.TransformDirection(targetTorque);
+        rb.AddTorque(targetTorque * speed);
     }
 }
